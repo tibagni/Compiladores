04912a67bd7aba99609128cffe2f3a80f78490d7
Tiago Bagni <tibagni@gmail.com>
2011-08-09 21:53:50 -0300

    Primeiro commit

---
+++ src/app/gui/DebuggerWindow.java
@@ -0,0 +1,22 @@
+package app.gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+@SuppressWarnings("serial")
+public class DebuggerWindow extends JFrame {
+    private JButton mRunBtn;
+    private JTextArea mOutputTxt;
+    
+    /**
+     * Consttrutor padrao. Cria layout da janela.
+     */
+    public DebuggerWindow() {
+        createMenu();
+    }
+
+    private void createMenu() {
+        
+    }
+}
